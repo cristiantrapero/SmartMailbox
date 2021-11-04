f2a8d964c1d7b8b0217ea32d72b48db5d2387af0
Cristian T <critramo@gmail.com>
2021-11-04 12:17:34 +0100

    add keypad support

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
 #include <Arduino.h>
+#include "Keypad.h"
+
+// Keypad size
+const byte ROWS = 4;
+const byte COLS = 4;
+
+// Keypad keys distribution
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+// Keypad pinout
+byte rowPins[ROWS] = {13, 12, 14, 27}; 
+byte colPins[COLS] = {26, 25, 33, 32};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000);
-  Serial.println("Hello world!");
+  char pressedKey = keypad.getKey();
+
+  if (pressedKey){
+    Serial.println(pressedKey);
+  }
 }
